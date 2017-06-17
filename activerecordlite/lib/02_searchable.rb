001f168427089b60eb4d81ee7de6110f456d836b
Nicholas Cannon <nickwcannon@gmail.com>
2017-06-16 18:37:14 -0700

    Active Record Lite

---
+++ activerecordlite/lib/02_searchable.rb
@@ -0,0 +1,28 @@
+require_relative 'db_connection'
+require_relative '01_sql_object'
+
+module Searchable
+  def where(params)
+
+
+    where_line = params.keys.map {|k| "#{k} = ?"}.join(" AND ")
+    values = params.map {|k,v| v}
+
+    result = DBConnection.execute(<<-SQL, *values)
+    SELECT
+      *
+    FROM
+      #{table_name}
+    WHERE
+      #{where_line}
+    SQL
+
+    parse_all(result)
+  end
+end
+
+
+
+class SQLObject
+  extend Searchable
+end
