a6ea23d3d49664c508a0b202d2f0caec6f4fc59c
inkymaze <nickwcannon@gmail.com>
2017-11-21 14:47:02 -0800

    add rearrange by k algorithm

--- interview_algos/linked_lists.rb
+++ interview_algos/linked_lists.rb
@@ -1,38 +1,38 @@
 # You're given 2 huge integers represented by linked lists. Each linked list element is a number from 0 to 9999 that represents a number with exactly 4 digits. The represented number might have leading zeros. Your task is to add up these huge integers and return the result in the same format.
-# 
+#
 # Example
-# 
+#
 # For a = [9876, 5432, 1999] and b = [1, 8001], the output should be
 # addTwoHugeNumbers(a, b) = [9876, 5434, 0].
-# 
+#
 # Explanation: 987654321999 + 18001 = 987654340000.
-# 
+#
 # For a = [123, 4, 5] and b = [100, 100, 100], the output should be
 # addTwoHugeNumbers(a, b) = [223, 104, 105].
-# 
+#
 # Explanation: 12300040005 + 10001000100 = 22301040105.
-# 
+#
 # Input/Output
-# 
+#
 # [time limit] 4000ms (rb)
 # [input] linkedlist.integer a
-# 
+#
 # The first number, without its leading zeros.
-# 
+#
 # Guaranteed constraints:
 # 0 ≤ a size ≤ 104,
 # 0 ≤ element value ≤ 9999.
-# 
+#
 # [input] linkedlist.integer b
-# 
+#
 # The second number, without its leading zeros.
-# 
+#
 # Guaranteed constraints:
 # 0 ≤ b size ≤ 104,
 # 0 ≤ element value ≤ 9999.
-# 
+#
 # [output] linkedlist.integer
-# 
+#
 # The result of adding a and b together, returned without leading zeros in the same format.
 
 # Definition for singly-linked list:
@@ -47,49 +47,49 @@
 def reverse(node)
  next_node = nil
   prev_node = nil
-  cur_node = node 
+  cur_node = node
    while cur_node
      next_node = cur_node.next
      cur_node.next = prev_node
      prev_node = cur_node
      cur_node = next_node
-   end 
+   end
   prev_node
-end 
+end
 
 def addTwoHugeNumbers(a, b)
   carry = 0
   out = ListNode.new(0)
-  head = out 
+  head = out
   a = reverse(a)
-  b = reverse(b) 
-  while a || b 
-    if a 
+  b = reverse(b)
+  while a || b
+    if a
       carry += a.value
       a = a.next
-    end 
-    if b 
-      carry += b.value 
+    end
+    if b
+      carry += b.value
       b = b.next
-    end 
+    end
     head.next = ListNode.new(carry%10000)
     head = head.next
     carry /= 10000
-          
-    if carry > 0 
+
+    if carry > 0
       head.next = ListNode.new(carry)
-    end       
-  end 
+    end
+  end
   reverse(out.next)
 end
 
 
 # Note: Your solution should have O(l1.length + l2.length) time complexity, since this is what you will be asked to accomplish in an interview.
-# 
+#
 # Given two singly linked lists sorted in non-decreasing order, your task is to merge them. In other words, return a singly linked list, also sorted in non-decreasing order, that contains the elements from both original lists.
-# 
+#
 # Example
-# 
+#
 # For l1 = [1, 2, 3] and l2 = [4, 5, 6], the output should be
 # mergeTwoLinkedLists(l1, l2) = [1, 2, 3, 4, 5, 6];
 # For l1 = [1, 1, 2, 4] and l2 = [0, 3, 5], the output should be
@@ -105,7 +105,7 @@ end
 #
 def mergeTwoLinkedLists(l1, l2)
    out = ListNode.new(0)
-   head = out 
+   head = out
    head1 = l1
    head2 = l2
   while head1 || head2
@@ -114,12 +114,12 @@ def mergeTwoLinkedLists(l1, l2)
         head.next = ListNode.new(head1.value)
         head = head.next
         head1 = head1.next
-      else 
+      else
         head.next = ListNode.new(head2.value)
         head = head.next
         head2 = head2.next
-      end 
-    elsif head1 
+      end
+    elsif head1
         head.next = ListNode.new(head1.value)
         head = head.next
         head1 = head1.next
@@ -127,7 +127,46 @@ def mergeTwoLinkedLists(l1, l2)
         head.next = ListNode.new(head2.value)
         head = head.next
         head2 = head2.next
-    end 
-  end 
+    end
+  end
   out.next
+end
+
+# Note: Try to solve this task in O(list size) time using O(1) additional space, since this is what you'll be asked during an interview.
+#
+# Given a singly linked list of integers l and a non-negative integer n, move the last n list nodes to the beginning of the linked list.
+#
+# Example
+#
+# For l = [1, 2, 3, 4, 5] and n = 3, the output should be
+# rearrangeLastN(l, n) = [3, 4, 5, 1, 2];
+# For l = [1, 2, 3, 4, 5, 6, 7] and n = 1, the output should be
+# rearrangeLastN(l, n) = [7, 1, 2, 3, 4, 5, 6].
+#
+# # Definition for singly-linked list:
+# # class ListNode
+# #   attr_accessor :value, :next
+# #   def initialize(val)
+# #     @value = val
+# #     @next = nil
+# # end
+
+ def rearrangeLastN(l, n)
+  return l if n == 0
+  fast, slow = l, l
+  n.times do
+    if fast
+      fast = fast.next
+    end
+  end
+  return l unless fast
+  while fast.next
+    slow = slow.next
+    fast = fast.next
+  end
+
+  result = slow.next
+  slow.next = nil
+  fast.next = l
+  result
 end
