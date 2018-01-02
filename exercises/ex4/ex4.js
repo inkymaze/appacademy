01bc21ea6cce91073578423312c8342bd8c17aaf
inkymaze <nickwcannon@gmail.com>
2018-01-02 12:40:35 -0800

    implement OLOO

--- exercises/ex4/ex4.js
+++ exercises/ex4/ex4.js
@@ -1,41 +1,51 @@
-function Widget(width,height) {
-	this.width = width || 50;
-	this.height = height || 50;
-	this.$elem = null;
-}
 
-Widget.prototype.render = function($where){
-	if (this.$elem) {
-		this.$elem.css({
-			width: this.width + "px",
-			height: this.height + "px"
-		}).appendTo($where);
+var Widget = {
+	init: function(width,height) {
+		this.width = width || 50;
+		this.height = height || 50;
+		this.$elem = null;
+	},
+	insert: function($where){
+		if (this.$elem) {
+			this.$elem.css({
+				width: this.width + "px",
+				height: this.height + "px"
+			}).appendTo($where);
+		}
 	}
 };
 
-function Button(/* ... */) {
-/*
-	...
+
+var Button = Object.create(Widget);
+
+
+Button.setup = function(width, height, label){
+	this.init(width, height);
+	this.label = label || "Default";
+	
 	this.$elem = $("<button>").text(this.label);
-*/
 }
 
-/*
-Button -> render = function($where) {
-	// call the parent render()
-	// add a click handler -> onClick
+Button.build = function($where) {
+	this.insert($where);
+	this.$elem.click(this.onClick.bind(this));
 }
 
-Button -> onClick = function(evt) {
-	console.log("...");
-}
+Button.onClick = function(evt) {
+	alert("Button: " + this.label + " has been clicked");
+	
+};
 
 $(document).ready(function(){
 	var $body = $(document.body);
-	var btn1 = ...;
-	var btn2 = ...;
+	
+	var btn1 = Object.create(Button);
+	btn1.setup(222,222,'Two');
+	
+	var btn2 = Object.create(Button);
+	btn2.setup(333,333,'Three');
+	
 
-	btn1.render($body);
-	btn2.render($body);
+	btn1.build($body);
+	btn2.build($body);
 });
-*/
